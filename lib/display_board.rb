6e5879a4bced4533c4b09f889ea92c040670c542
Richard Fenix <richard.fenix@gmail.com>
2018-03-27 03:39:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = [" "," "," "," "," "," "," "," "," "]
+#board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -11,8 +11,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-display_board(board)
+#display_board(board)
 
-board = [" ","X"," "," "," "," "," "," "," "]
+#board = [" ","X"," "," "," "," "," "," "," "]
 
-display_board(board)
+#display_board(board)
