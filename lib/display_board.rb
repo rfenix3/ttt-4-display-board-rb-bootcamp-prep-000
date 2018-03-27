5d4db488e084c12e369991684b9802fe86949902
Richard Fenix <richard.fenix@gmail.com>
2018-03-27 03:37:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,8 +11,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#display_board(board)
+display_board(board)
 
-#board = [" ","X"," "," "," "," "," "," "," "]
+board = [" ","X"," "," "," "," "," "," "," "]
 
 display_board(board)
